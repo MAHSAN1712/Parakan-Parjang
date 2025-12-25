183073878bc726c2bd53ea2d74a354f52025933a
MAHSAN1712 <m.ahsan070605@gmail.com>
2025-12-25 19:56:22 +0700

    update website

--- js/script.js
+++ js/script.js
@@ -5,13 +5,29 @@ const navbarNav = document.querySelector ('.navbar-nav');
 document.querySelector('#menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
+// Toogle 
+const navbarNavdollar = document.querySelector ('.dollar-cart');
+
+// ketika menu di click
+document.querySelector('#shop').onclick = () => {
+    navbarNavdollar.classList.toggle('active');
+};
 
 //Klike di luar sidebar menu 
 const Menu = document.querySelector ('#menu');
+const sb = document.querySelector ('#search');
+const sc = document.querySelector ('#shop');
+
 
 document.addEventListener('click', function (e) {
     if(!Menu.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active')
+    }
+    if(!sb.contains(e.target) && !navbarNavsearch.contains(e.target)){
+        navbarNavsearch.classList.remove('active')
+    }
+    if(!sc.contains(e.target) && !navbarNavdollar.contains(e.target)){
+        navbarNavdollar.classList.remove('active')
     }
 });
 
@@ -22,3 +38,36 @@ const navbarNavsearch = document.querySelector ('.search-form');
 document.querySelector('#search').onclick = () => {
     navbarNavsearch.classList.toggle('active');
 };
+
+// Modal Box 
+
+const itemdetailmodal = document.querySelector('#item-detail-modal')
+const itemdetailbuttons = document.querySelectorAll('.item-detail-button')
+
+itemdetailbuttons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemdetailmodal.style.display = 'flex';
+        e.preventDefault ();
+    }
+});
+
+itemdetailbuttons.onclick =(e)=> {
+    itemdetailmodal.style.display = 'flex';
+    e.preventDefault()
+};
+
+// Klik tombol close 
+
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemdetailmodal.style.display = 'none';
+     e.preventDefault();
+}
+
+// Close Modal 
+
+const modal = document.querySelector('#item-detail-modal')
+window.onclick = (e) => {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
